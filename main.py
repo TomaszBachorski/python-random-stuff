03e5fa6b10ef364aec179f9d8b19dbc620df9466
TomaszBachorski <tomekbachorski@o2.pl>
2022-07-09 17:24:40 +0200

    random search

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
 from random import randint
 from math import ceil
 
+
 def generateRandomNumber(minRange, maxRange):
     number = randint(minRange, maxRange)
     return number
 
+
 def binarySearch(minRange, maxRange, lookingFor):
     iterations = 0
-    while maxRange!=lookingFor and minRange!=lookingFor:
-        iterations+=1
-        middle = maxRange-((maxRange-minRange)/2)
-        if lookingFor>middle:
-            minRange = ceil(maxRange-(maxRange-minRange)/2)
+    while maxRange != lookingFor and minRange != lookingFor:
+        iterations += 1
+        middle = maxRange-(maxRange-minRange)/2
+        if lookingFor > middle:
+            minRange = ceil(middle)
             continue
-        if lookingFor<middle:
-            maxRange=ceil(maxRange-(maxRange-minRange)/2)
+        if lookingFor < middle:
+            maxRange = ceil(middle)
             continue
-        if lookingFor==middle:
+        if lookingFor == middle:
             break
     return iterations
+
+
+def randomSearch(minRange, maxRange, lookingFor):
+    iterations = 0
+    while maxRange != lookingFor and minRange != lookingFor:
+        iterations += 1
+        split = randint(minRange, maxRange)
+        if lookingFor > split:
+            minRange = split
+            continue
+        if lookingFor < split:
+            maxRange = split
+            continue
+        if lookingFor == split:
+            break
+    return iterations
+
 
 minRange = 0
 maxRange = 100
 
-randomNumber = generateRandomNumber(minRange,maxRange)
-print(randomNumber)
+binaryWins, randomWins, draws = 0, 0, 0
 
-binarySearchIterations = binarySearch(minRange, maxRange, randomNumber)
-print(binarySearchIterations)
+for i in range(10000):
+    lookingFor = generateRandomNumber(minRange, maxRange)
+    iterationsOfBinarySearch = binarySearch(minRange, maxRange, lookingFor)
+    iterationsOfRandomSearch = randomSearch(minRange, maxRange, lookingFor)
+    if iterationsOfBinarySearch < iterationsOfRandomSearch:
+        binaryWins += 1
+    if iterationsOfBinarySearch > iterationsOfRandomSearch:
+        randomWins += 1
+    if iterationsOfBinarySearch == iterationsOfRandomSearch:
+        draws += 1
+
+print("Binary search wins: ", binaryWins)
+print("Random search wins: ", randomWins)
+print("Draws: ", draws)
